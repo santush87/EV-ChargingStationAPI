045fe94db315c1a4a24e591504ee8b097c88e959
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-03-12 10:39:23 +0200

    First commit

---
+++ src/test/java/com/martin/aleksandrov/EVChargingStationAPI/EvChargingStationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martin.aleksandrov.EVChargingStationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvChargingStationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
