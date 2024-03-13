ab4eaf7b9eea1797cc109f445ae72f8211180db0
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-03-13 18:02:30 +0200

    Tests preparation

---
+++ src/test/java/com/martin/aleksandrov/EVChargingStationAPI/services/impl/ChargingStationServiceImplTest.java
@@ -0,0 +1,50 @@
+package com.martin.aleksandrov.EVChargingStationAPI.services.impl;
+
+import com.martin.aleksandrov.EVChargingStationAPI.models.dtos.ChargingStationDto;
+import com.martin.aleksandrov.EVChargingStationAPI.repositories.ChargingStationRepository;
+import com.martin.aleksandrov.EVChargingStationAPI.services.ChargingStationService;
+import org.apache.coyote.BadRequestException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ChargingStationServiceImplTest {
+
+    @Autowired
+    private ChargingStationService stationService;
+
+    @Autowired
+    private ChargingStationRepository stationRepository;
+
+    @BeforeEach
+    void setUp() {
+        this.stationRepository.deleteAll();
+    }
+
+    @AfterEach
+    void tearDown() {
+        this.stationRepository.deleteAll();
+    }
+
+    @Test
+    void testCreateChargingStationSuccessful() throws BadRequestException {
+//        ChargingStationDto stationTestDto = new ChargingStationDto();
+//        stationTestDto.setUniqueId("id_1");
+//        stationTestDto.setLatitude(64.246);
+//        stationTestDto.setLongitude(62.157);
+//        stationTestDto.setZipcode("5461");
+//
+////        The repository is empty and must be = 0
+//        assertEquals(0, this.stationRepository.count());
+//
+//
+////        Creating a new station and now the repository count must be 1
+//        this.stationService.createNewChargingStation(stationTestDto);
+//        assertEquals(1, this.stationRepository.count());
+    }
+}
